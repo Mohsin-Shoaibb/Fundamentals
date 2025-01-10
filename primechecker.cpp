ad92d5a5d99f5bd121b4aa5bd9db6cf320668973
Mohsin-Shoaibb <mohsinshoaib992@gmail.com>
2025-01-10 11:32:22 +0500

    Add new file

---
+++ primechecker.cpp
@@ -0,0 +1,32 @@
+//any number that is only divisible by 1 and itself is called prime. inshort, a prime is only divisible by 2 numbers
+//prime numbers are only positive and only even prime number is 2
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    //n is a variable to hold user input number
+    int n;
+    cout<<"Enter a number to check if it is prime: ";
+    cin>>n;
+    
+    int CountDivisors = 0;      // this variable counts how many numers divide the user input
+    for(int i = 1; i <= n; i++)
+    {
+        if(n%i == 0)
+        {
+           CountDivisors++;
+        }
+    }
+
+    if(prime == 2) 
+    {
+        cout<<n<<" is a prime number.\n";
+    }
+    else
+    {
+        cout<<n<<" is not a prime number.\n";
+    }
+
+    return 0;
+}
